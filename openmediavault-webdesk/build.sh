1f9a88012afdf2ab2a1a545087f6f3df30ef22cf
Antoine Weber <aweber@numesia.fr>
2018-11-13 18:10:57 +0100

    add links creation

--- openmediavault-webdesk/build.sh
+++ openmediavault-webdesk/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #sudo apt-get install build-essential devscripts debhelper
+rm ../openmediavault-webdesk_*
 debuild -us -uc
---
+++ openmediavault-webdesk/refresh-with-package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker-compose stop
+docker-compose rm -f
+docker-compose -f docker-compose-empty.yml up -d
+
+docker exec -t "omv" bash -c "dpkg -i openmediavault-webdesk_*_all.deb"
+
+
+echo "Attente du container OMV..."
+until nc -z $(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' omv) 80
+do
+    sleep 0.5
+done
+echo "OK"
+
+PORT=`docker inspect -f '{{json .NetworkSettings.Ports}}' omv | sed -e 's/.*"80\/tcp[^}]*HostPort":"//g' | sed -e 's/".*//g'`
+
+echo -e "\033[32m"
+echo -e "Votre environnement est maintenant disponible, à cette adresse: "
+echo -e "http://localhost:$PORT"
